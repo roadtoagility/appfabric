c181119269f8157f832fbb66a211dcf5d4a03f14
drr00t <adribeiro@gmail.com>
2021-12-31 11:06:28 -0300

    tests for billing

---
+++ sources/AppFabric.Tests/Domain/Data/GenerateReleaseDifferentClientsTestingData.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using AppFabric.Domain.BusinessObjects;
+
+namespace AppFabric.Tests.Domain.Data
+{
+    public class GenerateReleaseDifferentClientsTestingData:IEnumerable<object[]>
+    {
+        private static readonly EntityId ClientId = EntityId.GetNext();
+        private readonly List<object[]> _data = new List<object[]>
+        {
+            new object[]
+            {
+                ClientId,
+                Release.NewRequest(EntityId.GetNext()),
+                Release.NewRequest(EntityId.GetNext())
+            }
+        };
+
+        public IEnumerator<object[]> GetEnumerator() => _data.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
